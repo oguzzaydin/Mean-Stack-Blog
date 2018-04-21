fb92eb686be1112004acadc1228b29d996934ff9
Oğuzhan Aydın <oguzhanaydin56@gmail.com>
2018-04-21 20:16:14 +0300

    register user

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
 
-import {AppRoutingModule} from './app-rouinting.module'
+import { AppRoutingModule } from './app-rouinting.module'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service'
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    DashboardComponent
+    RegisterComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
